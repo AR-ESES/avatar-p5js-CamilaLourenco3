02b7a1a00960268cf21bbb912545766c799e3080
Camila Lourenço <220000606@ese.ipsantarem.pt>
2024-04-02 16:41:47 +0100

    exercicio avatar

--- sketch.js
+++ sketch.js
@@ -1,11 +1,22 @@
 function setup() {
-  createCanvas(1280, 1024);
-  background(153);
-  stroke(0);
-  strokeWeight(2);
-  fill(237, 28, 36);
-  circle(30, 30, 40);
-  line(30, 30, 1280 / 2, 1024 / 2);
+  createCanvas(1920, 1080);
 }
 
-function draw() {}
+function draw() {
+  rect(1920/2,1080/2,40,80)
+  fill (50,50,10)
+  stroke(2)
+  rect(920,620,40,10)
+  fill (50,50,10)
+  stroke(2)
+  rect(960,620,40,10)
+  fill (255,25,25)
+  stroke(2)
+  rect(920,430,80,110)
+  fill (255,25,25)
+  stroke(2)
+  rect(890,430,30,90)
+  fill (255,25,25)
+  stroke(2)
+  rect(1000,430,30,90)
+}
